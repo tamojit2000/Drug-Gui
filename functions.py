f13c2df2ced6b6d456c1f1595a6a1243c4cb63b4
tamojit2000 <tamojitdas2000@gmail.com>
2021-01-20 19:11:34 +0530

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,55 @@
+from lime import lime_image
+from skimage.segmentation import mark_boundaries
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications import vgg16 as inc_net
+import tensorflow
+import numpy as np
+import matplotlib.pyplot as plt
+
+print('Modules loaded')
+
+## BEGIGN 0
+## MALIGNANT 1
+## NORMAL 2
+
+Labels={
+    0:'BEGIGN',
+    1:'MALIGNANT',
+    2:'NORMAL'
+    }
+
+
+def transform_img_fn(path_list):
+    out = []
+    for img_path in path_list:
+        img = image.load_img(img_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = inc_net.preprocess_input(x)
+        out.append(x)
+    return np.vstack(out)
+
+def get_explanation():
+    model = tensorflow.keras.models.load_model('Model_1.h5')
+    print(1)
+    images = transform_img_fn(['test.png'])
+    print(2)
+    explainer = lime_image.LimeImageExplainer()
+    print(3)
+    explanation = explainer.explain_instance(images[0].astype('double'), model.predict, top_labels=5, hide_color=0, num_samples=1000)
+    print(4)
+    temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=True)
+    print(5)
+    plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+
+
+def get_prediction(path):
+    model = tensorflow.keras.models.load_model('Model_1.h5')
+    print(1)
+    images = transform_img_fn([path])
+    print(2)
+    return Labels[np.argmax(model.predict(images))]
+
+#get_explanation()
+
+#get_prediction()
---
+++ main.py
@@ -0,0 +1,43 @@
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
+import sys
+import cv2
+from functions import *
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('basic.ui', self)
+        self.PATH=None
+        self.pushButton.clicked.connect(self.browse)
+        self.pushButton_2.clicked.connect(self.Predict)
+        self.pushButton_3.clicked.connect(self.About)
+        self.show()
+
+    def browse(self):
+        fm = QtWidgets.QFileDialog.getOpenFileName(None,'Browse File')
+        filename = fm[0]
+        if filename=='': filename='a.jpg'                
+        self.label.setPixmap(QtGui.QPixmap(filename))
+        self.PATH=filename
+        print('File Path:',self.PATH)
+        self.label_4.setText('Result')
+
+    def Predict(self):
+        print('predict')
+        print('File Path:',self.PATH)
+        self.label_4.setText('Processing')
+        prediction=get_prediction(self.PATH)
+        print('Prediction:',prediction)
+        self.label_4.setText(prediction)
+
+    def About(self):
+        print('About')
+        msg = QtWidgets.QMessageBox()
+        msg.setWindowTitle("About")
+        msg.setText("About Text.")
+        msg.setIcon(QtWidgets.QMessageBox.Information)
+        msg.exec_()
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
